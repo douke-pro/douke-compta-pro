cf1675c98ec8084ab0b8d8a3737a891fde6e6951
DOUKE Pro <doukepro@gmail.com>
2025-12-15 01:22:07 +0100

    Initialize company routes in company.js

---
+++ routes/company.js
@@ -0,0 +1,8 @@
+// Fichier : routes/company.js
+const express = require('express');
+const router = express.Router();
+
+// Route de test ou placeholder
+// router.get('/', (req, res) => res.send('Company routes working'));
+
+module.exports = router;
