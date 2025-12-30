770b502db4d91091656da153a05f9e66abdcfea6
DOUKE Pro <doukepro@gmail.com>
2025-12-30 19:14:01 +0100

    Create userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+// =============================================================================
+// FICHIER : routes/userRoutes.js
+// =============================================================================
+
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/auth');
+const userController = require('../controllers/userController');
+
+// Route pour le tableau de bord ou les données de session initiales
+router.get('/session-data', protect, userController.getSessionData);
+
+module.exports = router;
