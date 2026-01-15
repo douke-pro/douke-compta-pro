7dcd9169da1664129678af0f3055beecc15a5032
DOUKE Pro <doukepro@gmail.com>
2026-01-15 02:15:28 +0100

    Create __manifest__.py for Douke Compta Pro API
    Added manifest details for Douke Compta Pro API.

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,11 @@
-
+# -*- coding: utf-8 -*-
+{
+    'name': 'Douke Compta Pro API',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'API de liaison pour Node.js',
+    'depends': ['account'],  # Indispensable pour modifier account.move
+    'data': [],
+    'installable': True,
+    'application': True,
+}
