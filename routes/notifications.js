a2c668308a7ba92926644ebc926bb763adf61e70
DOUKE Pro <doukepro@gmail.com>
2026-02-09 03:07:45 +0100

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -5,3 +5,15 @@ router.post(
     restrictTo('ADMIN', 'COLLABORATEUR'), // Middleware de restriction
     notificationsController.sendNotification
 );
+
+const express = require('express');
+const router = express.Router();
+const { protect, checkCompanyAccess } = require('../middleware/auth');
+const notificationsController = require('../controllers/notificationsController');
+
+router.get('/', protect, checkCompanyAccess, notificationsController.getNotifications);
+router.post('/send', protect, checkCompanyAccess, notificationsController.sendNotification);
+router.patch('/:id/read', protect, notificationsController.markAsRead);
+router.delete('/:id', protect, notificationsController.deleteNotification);
+
+module.exports = router;
