1ed506c4c3cbd12c7de635d916b5105c688fb160
DOUKE Pro <doukepro@gmail.com>
2026-02-10 22:34:41 +0100

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { protect, checkCompanyAccess } = require('../middleware/auth');
 const notificationsController = require('../controllers/notificationsController');
+const { authenticateToken } = require('../middleware/auth');
 
 router.get('/', protect, checkCompanyAccess, notificationsController.getNotifications);
 router.post('/send', protect, checkCompanyAccess, notificationsController.sendNotification);
 router.patch('/:id/read', protect, notificationsController.markAsRead);
 router.delete('/:id', protect, notificationsController.deleteNotification);
+router.patch('/:id/read', authenticateToken, notificationsController.markNotificationAsRead);
 
 module.exports = router;
